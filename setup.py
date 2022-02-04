559727e350306fb9e608afa78ecee0a725eb9d90
tkerscher <88444139+tkerscher@users.noreply.github.com>
2022-02-04 23:38:18 +0100

    Renamed to BlaST

--- setup.py
+++ setup.py
@@ -4,23 +4,23 @@ def readme():
     with open('README.md') as f:
         return f.read()
 
-setup(name='blase',
-    version='0.3.1',
+setup(name='astroblast',
+    version='1.0.0',
     description='Synchtrotron peak estimator for blazars',
     long_description='Estimates the synchrotron peak of blazars with prediction interval given a sed as produced by the VOUBlazar tool. Uses an ensemble of neural networks powered by pytorch.',
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
         'Topic :: Scientific/Engineering :: Astronomy'
     ],
     keywords='blazar synchrotron',
-    url='https://github.com/tkerscher/blase',
+    url='https://github.com/tkerscher/blast',
     author='Tobias Kerscher',
     author_email='88444139+tkerscher@users.noreply.github.com',
     license='MIT',
-    packages=['blase'],
+    packages=['blast'],
     install_requires=[
         'numpy',
         'scipy',
@@ -28,7 +28,7 @@ setup(name='blase',
         'tqdm'
     ],
     entry_points={
-        'console_scripts': ['blase=blase.blase:main']
+        'console_scripts': ['blast=blast.blast:main']
     },
     include_package_data=True,
     zip_safe=False)
